71ecc489c0b04f327a88257e70e3b680e1a14d3c
chapes-gh <jsearls90@gmail.com>
2024-03-10 11:42:44 -0400

    refactor file read into get_book_contents function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+def get_book_contents(file_path):
+    with open(file_path) as file:
+        return file.read()
+
 def get_word_count(text):
     return len(text.split(" "))
 
 def main():
-    file_contents = ""
-    with open("books/frankenstein.txt") as file:
-        file_contents = file.read()
-    
-    word_count = get_word_count(file_contents)
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_contents(book_path)
+    word_count = get_word_count(book_contents)
 
     print(f"Word Count: {word_count}")
 
